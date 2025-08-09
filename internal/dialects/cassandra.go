b59aba4fa87e31d00e37fc392a4132677950cb01
nileric <kvashin.am98@gmail.com>
2025-08-09 23:01:21 +0700

    impl querier

--- internal/dialects/cassandra.go
+++ internal/dialects/cassandra.go
@@ -1,6 +1,8 @@
 package dialects
 
 import (
+	"fmt"
+
 	"github.com/pressly/goose/v3/database/dialect"
 )
 
@@ -14,25 +16,38 @@ type cassandra struct{}
 var _ dialect.Querier = (*cassandra)(nil)
 
 func (c *cassandra) CreateTable(tableName string) string {
-	panic("unimplemented")
-}
-
-func (c *cassandra) DeleteVersion(tableName string) string {
-	panic("unimplemented")
-}
-
-func (c *cassandra) GetLatestVersion(tableName string) string {
-	panic("unimplemented")
-}
-
-func (c *cassandra) GetMigrationByVersion(tableName string) string {
-	panic("unimplemented")
+	q := `CREATE TABLE %s (
+		pk int,
+		version_id bigint,
+		is_applied boolean,
+		tstamp timestamp,
+		PRIMARY KEY (pk, version_id)
+	) WITH CLUSTERING ORDER BY (version_id DESC)`
+	return fmt.Sprintf(q, tableName)
 }
 
 func (c *cassandra) InsertVersion(tableName string) string {
-	panic("unimplemented")
+	q := `INSERT INTO %s (pk, version_id, is_applied, tstamp) VALUES (1, ?, ?, toTimestamp(now()))`
+	return fmt.Sprintf(q, tableName)
+}
+
+func (c *cassandra) DeleteVersion(tableName string) string {
+	q := `DELETE FROM %s WHERE pk=1 AND version_id=?`
+	return fmt.Sprintf(q, tableName)
+}
+
+func (c *cassandra) GetMigrationByVersion(tableName string) string {
+	q := `SELECT tstamp, is_applied FROM %s WHERE pk=1 AND version_id=? LIMIT 1`
+	return fmt.Sprintf(q, tableName)
 }
 
 func (c *cassandra) ListMigrations(tableName string) string {
-	panic("unimplemented")
+	// Due to CLUSTERING ORDER BY (version_id DESC), this returns descending order.
+	q := `SELECT version_id, is_applied FROM %s WHERE pk=1`
+	return fmt.Sprintf(q, tableName)
+}
+
+func (c *cassandra) GetLatestVersion(tableName string) string {
+	q := `SELECT max(version_id) FROM %s WHERE pk=1`
+	return fmt.Sprintf(q, tableName)
 }
